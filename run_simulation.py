c507c1d353d77e4ca1e99c67c673096585b6a838
Bart Geurten <bart.geurten@otago.ac.nz>
2023-11-19 13:12:36 +1300

    simpler runs forn faster testeing

--- run_simulation.py
+++ run_simulation.py
@@ -4,8 +4,8 @@ from tqdm import tqdm
 
 # Simulation parameters
 grid_size = 500  # Adjust as needed
-initial_population = 100  # Adjust as needed
-wolbachia_effects = {'cytoplasmic_incompatibility': True, 'male_killing': True, 'increased_exploration_rate': True,'increased_eggs':True}
+initial_population = 50 # Adjust as needed
+wolbachia_effects = {'cytoplasmic_incompatibility': True, 'male_killing': True, 'increased_exploration_rate': False,'increased_eggs':True}
 infected_fraction = 0.2  # 10% initially infected
 
 # Create the environment
